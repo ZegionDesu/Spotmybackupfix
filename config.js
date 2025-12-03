dfa832870722d5e442c28500554b764254baf848
ZegionDesu <okie4468@gmail.com>
2025-12-03 23:02:10 +0700

    Update URI in config.js for new endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 config = {
-    uri: "https://zegiondesu.github.io",
+    uri: "https://zegiondesu.github.io/Spotmybackupfix",
     redirect_uri: "https://zegiondesu.github.io/Spotmybackupfix/callback",
     client_id: "f45e7a2b9d5b4e708d261f46313c4cd4",
     slowdown_import: 100,
