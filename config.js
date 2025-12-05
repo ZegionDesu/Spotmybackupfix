f6791f3218333fa86015e40089504d994b6d77b6
ZegionDesu <okie4468@gmail.com>
2025-12-05 19:29:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 config = {
-    client_id: "f45e7a2b9d5b4e708d261f46313c4cd4",
-    redirect_uri: "https://zegiondesu.github.io/Spotmybackupfix/callback",
+  client_id: "f45e7a2b9d5b4e708d261f46313c4cd4",
+  redirect_uri: "https://zegiondesu.github.io/Spotmybackupfix/callback",
+  origin: "https://zegiondesu.github.io",
+  slowdown_import: 200,
+  slowdown_export: 200
 };
